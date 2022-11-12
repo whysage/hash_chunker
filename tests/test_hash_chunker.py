473645a745f3819f85558d1e2e04cb99e4ef5b93
MaxiTDI <pyshniakm91@gmail.com>
2022-11-12 14:36:02 +0200

    add exceptions

--- tests/test_hash_chunker.py
+++ tests/test_hash_chunker.py
@@ -5,26 +5,28 @@ import pytest
 
 from hash_chunker import HashChunker
 
+from contextlib import nullcontext as does_not_raise
+
 
 @pytest.mark.parametrize(
     "chunk_size, all_items_count, expected",
     [
         (1000, 2000, [("", "8000000000"), ("8000000000", "ffffffffff")]),
         (
-            500,
-            1500,
-            [
-                ("", "5555600000"),
-                ("5555600000", "aaaac00000"),
-                ("aaaac00000", "ffffffffff"),
-            ],
+                500,
+                1500,
+                [
+                    ("", "5555600000"),
+                    ("5555600000", "aaaac00000"),
+                    ("aaaac00000", "ffffffffff"),
+                ],
         ),
     ],
 )
 def test_default_usage(
-    chunk_size: int,
-    all_items_count: int,
-    expected: List[Tuple[str, str]],
+        chunk_size: int,
+        all_items_count: int,
+        expected: List[Tuple[str, str]],
 ) -> None:
     """
     Simple test.
@@ -43,10 +45,10 @@ def test_default_usage(
     ],
 )
 def test_chunk_hash_length(
-    chunk_size: int,
-    all_items_count: int,
-    chunk_hash_length: int,
-    expected: List[Tuple[str, str]],
+        chunk_size: int,
+        all_items_count: int,
+        chunk_hash_length: int,
+        expected: List[Tuple[str, str]],
 ) -> None:
     """
     Test chunk_hash_length option.
@@ -65,18 +67,18 @@ def test_chunk_hash_length(
     [
         (2, [("", "8000000000"), ("8000000000", "ffffffffff")]),
         (
-            3,
-            [
-                ("", "5555600000"),
-                ("5555600000", "aaaac00000"),
-                ("aaaac00000", "ffffffffff"),
-            ],
+                3,
+                [
+                    ("", "5555600000"),
+                    ("5555600000", "aaaac00000"),
+                    ("aaaac00000", "ffffffffff"),
+                ],
         ),
     ],
 )
 def test_get_fixed_chunks(
-    chunks_count: int,
-    expected: List[Tuple[str, str]],
+        chunks_count: int,
+        expected: List[Tuple[str, str]],
 ) -> None:
     """
     Simple test.
@@ -85,3 +87,33 @@ def test_get_fixed_chunks(
     :param expected: expected chunks
     """
     assert list(HashChunker().get_fixed_chunks(chunks_count)) == expected
+
+
+@pytest.mark.parametrize(
+    "test_arg, expected",
+    [
+        (3, does_not_raise()),
+        (True, pytest.raises(TypeError)),
+        (5.45, pytest.raises(TypeError)),
+        (-5, pytest.raises(ValueError)),
+        ('3', pytest.raises(TypeError)),
+    ]
+)
+def test_exception_(test_arg, expected):
+    with expected:
+        assert HashChunker()._exceptions_(test_arg) is None
+
+
+@pytest.mark.parametrize(
+    "test_arg, expected",
+    [
+        (2, does_not_raise()),
+        (False, pytest.raises(TypeError)),
+        (257.49, pytest.raises(TypeError)),
+        (0, does_not_raise()),
+        ('arg', pytest.raises(TypeError)),
+    ]
+)
+def test_except_all_items_count(test_arg, expected):
+    with expected:
+        assert HashChunker()._except_all_items_count(test_arg) is None
